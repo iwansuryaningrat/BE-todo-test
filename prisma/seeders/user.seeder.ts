cf17aa8c7a63d7908745e9c01c091a56b6923454
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2025-06-29 04:19:14 +0700

    fix user seeder

--- prisma/seeders/user.seeder.ts
+++ prisma/seeders/user.seeder.ts
@@ -3,11 +3,12 @@ import { faker } from "@faker-js/faker";
 export const userSeeder = () => {
   let users = [];
   for (let i = 0; i < 5; i++) {
+    const email = faker.internet.email();
     users.push({
       username: faker.internet.username(),
       name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.email()
+      email,
+      password: email
     })
   }
   return users;
